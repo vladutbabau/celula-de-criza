44b6dba5808c0c9729228df77052efc020985514
Băbău Vlad - Andrei <vladut_babau@yahoo.com>
2025-07-24 15:57:49 +0300

    modificat intersection observer

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ const revealSection = function (entries, observer) {
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.2,
+  threshold: 0,
 });
 
 allSections.forEach((section) => {
